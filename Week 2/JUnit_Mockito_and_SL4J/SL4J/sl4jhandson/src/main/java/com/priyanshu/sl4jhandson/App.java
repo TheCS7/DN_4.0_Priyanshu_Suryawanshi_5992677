d116d41d4d97da3a9286947d5020eaaaa8cdb4d4
Priyanshu <suryawanshi.priyanshu1122@gmail.com>
2025-06-29 19:30:53 +0530

    Sl4j

---
+++ Week 2/JUnit_Mockito_and_SL4J/SL4J/sl4jhandson/src/main/java/com/priyanshu/sl4jhandson/App.java
@@ -0,0 +1,10 @@
+package com.priyanshu.sl4jhandson;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
