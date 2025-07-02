60b7e006284e47a9e60a2f36ebfc30d6ff824539
Priyanshu <suryawanshi.priyanshu1122@gmail.com>
2025-07-03 02:00:04 +0530

    word file update e1

--- Week 3/Spring_Core_Maven_Handson/E1_Configuring_Basic_Spring_Applicaiton/LibraryManagement/src/main/java/com/exercise1/LibraryManagement/LibraryManagementApplication.java
+++ Week 3/Spring_Core_Maven_Handson/E1_Configuring_Basic_Spring_Applicaiton/LibraryManagement/src/main/java/com/exercise1/LibraryManagement/LibraryManagementApplication.java
@@ -1,13 +1,14 @@
 package com.exercise1.LibraryManagement;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class LibraryManagementApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementApplication.class, args);
-	}
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+		BookService bookService = context.getBean("bookService", BookService.class);
+		bookService.performOperation();
+	}
 }
--- Week 3/Spring_Core_Maven_Handson/E1_Configuring_Basic_Spring_Applicaiton/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week 3/Spring_Core_Maven_Handson/E1_Configuring_Basic_Spring_Applicaiton/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,11 +1,7 @@
 package com.library.service;
 
-import com.library.repository.BookRepository;
-
 public class BookService {
-    private BookRepository bookRepository;
-
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public void performOperation() {
+        System.out.println("BookService: Performing book operation...");
     }
 }
